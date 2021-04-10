5af234c2c4e81a940eb064b04a96cddfd258fea1
Jose Leonardo Quiñones <56600373+joseleoo@users.noreply.github.com>
2021-04-10 09:48:05 -0500

    terminados los primeros componentes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <main>
     <px-header></px-header>
+    <px-assets-table></px-assets-table>
   </main>
 </template>
 <script>
 import PxHeader from "@/components/PxHeader";
+import PxAssetsTable from "@/components/PxAssetsTable";
 
 export default {
   name: "App",
-  components: { PxHeader },
+  components: { PxHeader, PxAssetsTable },
 };
 </script>
 
