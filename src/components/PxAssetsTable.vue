5ff1361afb3d973beac408a68355287104f893d5
Jose Leonardo Quiñones <56600373+joseleoo@users.noreply.github.com>
2021-05-12 16:28:25 -0500

    terminado

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -3,28 +3,36 @@
     <thead>
       <tr class="bg-gray-100 border-b-2 border-gray-400">
         <th></th>
-        <th>
-          <span>Ranking</span>
+        <th :clase="{ up: this.sortOrder === 1, down: this.sortOrder === -1 }">
+          <span class="underline cursor-pointer" @click="changeSortOrder"
+            >Ranking</span
+          >
         </th>
         <th>Nombre</th>
         <th>Precio</th>
         <th>Cap. de Mercado</th>
         <th>Variación 24hs</th>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <input
+            class="bg-gray-100 focus:outline-none border-b border-gray-400 py-2 px-4 block w-full appearance-none leading-normal"
+            id="filter"
+            placeholder="Buscar..."
+            type="text"
+            v-model="filter"
+          />
+        </td>
       </tr>
     </thead>
     <tbody>
       <tr
-        v-for="a in assets"
+        v-for="a in filteredAssets"
         :key="a.id"
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
       >
         <td>
           <img
             class="w-6 h-6"
-            :src="
-              `https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`
-            "
+            :src="`https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`"
             :alt="a.name"
           />
         </td>
@@ -61,35 +69,63 @@
 </template>
 
 <script>
-import PxButton from '@/components/PxButton'
+import PxButton from "@/components/PxButton";
 
 export default {
-  name: 'PxAssetsTable',
+  name: "PxAssetsTable",
 
   components: { PxButton },
+  data() {
+    return {
+      filter: "",
+      sortOrder: 1,
+    };
+  },
 
   props: {
     assets: {
       type: Array,
-      default: () => []
-    }
+      default: () => [],
+    },
+  },
+
+  computed: {
+    filteredAssets() {
+      const altOrder = this.sortOrder === 1 ? -1 : 1;
+
+      return this.assets
+        .filter(
+          (a) =>
+            a.symbol.toLowerCase().includes(this.filter.toLowerCase()) ||
+            a.name.toLowerCase().includes(this.filter.toLowerCase())
+        )
+        .sort((a, b) => {
+          if (parseInt(a.rank) > parseInt(b.rank)) {
+            return this.sortOrder;
+          }
+          return altOrder;
+        });
+    },
   },
 
   methods: {
     goToCoin(id) {
-      this.$router.push({ name: 'coin-detail', params: { id } })
+      this.$router.push({ name: "coin-detail", params: { id } });
+    },
+    changeSortOrder(){
+      this.sortOrder = this.sortOrder === 1 ? -1 : 1;
     }
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
 .up::before {
-  content: '👆';
+  content: "👆";
 }
 
 .down::before {
-  content: '👇';
+  content: "👇";
 }
 
 td {
